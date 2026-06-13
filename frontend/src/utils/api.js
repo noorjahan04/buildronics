105b6a3291d037b4c5da3cba839d388e8c512b24
Noor jahan <noorifms@gmail.com>
2026-06-13 10:48:41 +0530

    updated .env

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,22 +1,35 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: '/api' });
-
-API.interceptors.request.use(config => {
-  const token = localStorage.getItem('token');
-  if (token) config.headers.Authorization = `Bearer ${token}`;
-  return config;
+const API = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || 'https://buildronics.onrender.com/api'
 });
 
+API.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
 API.interceptors.response.use(
-  res => res,
-  err => {
-    if (err.response?.status === 401) {
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
       localStorage.removeItem('token');
       localStorage.removeItem('user');
-      window.location.href = '/login';
+
+      if (window.location.pathname !== '/login') {
+        window.location.href = '/login';
+      }
     }
-    return Promise.reject(err);
+
+    return Promise.reject(error);
   }
 );
 
